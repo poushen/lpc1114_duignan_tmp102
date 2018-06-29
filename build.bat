5949b592240dfa1bcfb68aaefe81cdce8ceb5e83
Phou-shen Ou <31812245+poushen@users.noreply.github.com>
2018-06-30 07:21:03 +0800

    remove map file

--- build.bat
+++ build.bat
@@ -11,5 +11,5 @@ arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -I. -g -c .\driver\oled_font.c -o .\ou
 arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -I. -g -c .\driver\i2c_oled.c -o .\out\i2c_oled.o
 arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -I. -g -c .\driver\i2c_tmp102.c -o .\out\i2c_tmp102.o
 arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -I. -I.\driver -g -c main.c -o .\out\main.o
-arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb .\out\init.o .\out\led.o .\out\uart.o .\out\systick.o .\out\i2c.o .\out\i2c_eeprom.o .\out\ftoa.o .\out\oled_font.o .\out\i2c_oled.o .\out\i2c_tmp102.o .\out\main.o -L "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\lib\gcc\arm-none-eabi\5.4.1\armv6-m" -lgcc -T linker_script.ld -Wl,-Map=main.map -nostartfiles -o .\out\main.elf
+arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb .\out\init.o .\out\led.o .\out\uart.o .\out\systick.o .\out\i2c.o .\out\i2c_eeprom.o .\out\ftoa.o .\out\oled_font.o .\out\i2c_oled.o .\out\i2c_tmp102.o .\out\main.o -L "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\lib\gcc\arm-none-eabi\5.4.1\armv6-m" -lgcc -T linker_script.ld -Wl,-Map=.\out\main.map -nostartfiles -o .\out\main.elf
 arm-none-eabi-objcopy -O ihex .\out\main.elf .\out\main.hex
